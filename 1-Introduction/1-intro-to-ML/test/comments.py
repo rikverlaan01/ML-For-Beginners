a00c0820614a5b53751dc5c9064f15ca64479592
Rik Verlaan <rikverlaan01@gmail.com>
2025-12-15 01:02:52 +0100

    small update

---
+++ 1-Introduction/1-intro-to-ML/test/comments.py
@@ -0,0 +1,3 @@
+# This line of code does absolutely do nothing
+print("This is a man's world")
+#  print('this is a man's world')
---
+++ 1-Introduction/1-intro-to-ML/test/test.py
@@ -0,0 +1,3 @@
+name = input('Type in your name: ')
+print('My name is:')
+print(name)
